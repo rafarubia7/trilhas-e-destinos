ac8652b1e23d52eb9e919c792cec994bf78f37e7
Rafael Rubiá Oliveira Cardoso <166622515+rafarubia7@users.noreply.github.com>
2024-10-09 13:44:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Função para abrir o modal
+
 function openModal(modalId) {
     document.getElementById(modalId).style.display = "block";
 }
 
-// Função para fechar o modal
+
 function closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
 }
 
-// Fechar o modal ao clicar fora do conteúdo
+
 window.onclick = function(event) {
     if (event.target.classList.contains('modal')) {
         closeModal(event.target.id);
